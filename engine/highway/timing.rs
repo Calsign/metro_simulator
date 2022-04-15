ffb0a47d060d67da49f3d0248decca77511cff15
Will Smith <wds68@cornell.edu>
2022-04-14 21:53:40 -0700

    increase default highway speed
    Not all highways in OSM have speed limits. We were previously assuming ~25 mph.
    In practice, most highways are actual motorways, so increased speed limit to 60 mph.

--- engine/highway/timing.rs
+++ engine/highway/timing.rs
@@ -1,7 +1,7 @@
 use crate::segment::HighwaySegment;
 
 // if a highway doesn't have a known speed limit, we use an assumed speed
-const DEFAULT_SPEED: u32 = 11; // ~25 mph
+const DEFAULT_SPEED: u32 = 27; // ~60 mph
 
 pub fn travel_time(segment: &HighwaySegment, tile_size: f64) -> f64 {
     let speed = segment.data.speed_limit.unwrap_or(DEFAULT_SPEED) as f64;
